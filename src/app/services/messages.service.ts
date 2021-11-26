c6ca6f1130576d08f0e238e61d0acc0e368320d2
Tomas Danelutti <tomasdanelutti1@hotmail.com>
2021-11-26 19:25:44 -0300

    Agrego servicio de mensajes

---
+++ src/app/services/messages.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  toast: HTMLIonToastElement;
+
+  constructor(private toastController: ToastController, private alertController: AlertController) { }
+
+  async showMessage(header: string, message: string, duration: number) {
+
+    if(this.toast) {
+      // Si ya existe un toast lo cierro, para mostrar el nuevo.
+      this.toast.dismiss();
+    }
+
+    this.toast = await this.toastController.create({
+      header: header,
+      message: message,
+      duration: duration,
+      buttons: [
+        {
+          text: 'Cerrar',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    this.toast.present();
+  }
+
+  async showAlert(header: string, message: string, handler: any) {
+
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: handler,
+        },{
+          text: 'Cancelar',
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+}
