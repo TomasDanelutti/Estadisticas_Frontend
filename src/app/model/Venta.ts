2c2c0bb92479e3d7c098c14888768e46d3eac4a1
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-08-07 19:14:24 -0300

    cambio

--- src/app/model/Venta.ts
+++ src/app/model/Venta.ts
@@ -1,11 +1,9 @@
 import {Vendedor} from './Vendedor';
-import {Mes} from './Mes';
 import {Producto} from './Producto';
 
 export class Venta {
     idVenta: number;
     vendedor: Vendedor;
-    mes: Mes;
     producto: Producto[];
     cantProductos: number;
     total: number;
